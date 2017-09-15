b8f56f510af94c7e95e402cc8996cee1727fc4af
Thomas Hillyer <thomashillyer@users.noreply.github.com>
2017-09-14 20:00:49 -0400

    Merge EJ Code

--- Lab1_Haykal_Hillyer/src/ca/mcgill/ecse211/wallfollowing/BangBangController.java
+++ Lab1_Haykal_Hillyer/src/ca/mcgill/ecse211/wallfollowing/BangBangController.java
@@ -4,13 +4,16 @@ import lejos.hardware.motor.*;
 
 public class BangBangController implements UltrasonicController {
 
+	private static final int FILTER_OUT = 20;
+	private int filterControl;
+
 	private final int bandCenter;
 	private final int bandwidth;
 	private final int motorLow;
 	private final int motorHigh;
 	private int distance;
-	
-	//Distance constants
+
+	// Distance constants
 	private static final int tinyRangeDist = 10;
 	private static final int minRangeDist = 20;
 	private static final int maxRangeDist = 30;
@@ -26,43 +29,89 @@ public class BangBangController implements UltrasonicController {
 		WallFollowingLab.leftMotor.forward();
 		WallFollowingLab.rightMotor.forward();
 	}
-	
+
 	@Override
 	public void processUSData(int distance) {
-		this.distance = distance;
+		// this.distance = distance;
 		// TODO: process a movement based on the us distance passed in (BANG-BANG style)
 
-		
-		//TODO: there are some errors when it gets within the minimum range that the sensor can handle, the "dead width" where it will run into the wall
-		if (minRangeDist < distance && distance < maxRangeDist) { // Robot is in the acceptable range
+		// EJ
+		if (distance >= 255 && filterControl < FILTER_OUT) {
+			// bad value, do not set the distance var, however do increment the
+			// filter value
+			filterControl++;
+		} else if (distance >= 255) {
+			// We have repeated large values, so there must actually be nothing
+			// there: leave the distance alone
+			this.distance = distance;
+		} else {
+			// distance went below 255: reset filter and leave
+			// distance alone.
+			filterControl = 0;
+			this.distance = distance;
+		}
 
-			WallFollowingLab.leftMotor.setSpeed(motorHigh);
+		float floatDistance = this.distance / (float) 1.4;
+
+		this.distance = (int) floatDistance;
+
+		if (this.distance > bandCenter + bandwidth) { // Too far from wall on left
+
+			WallFollowingLab.leftMotor.setSpeed(motorLow); // Turn Left
 			WallFollowingLab.rightMotor.setSpeed(motorHigh);
 			WallFollowingLab.leftMotor.forward();
 			WallFollowingLab.rightMotor.forward();
 
-		} else if (distance < tinyRangeDist) { // Robot way too close to the wall on the left
+		} else if (this.distance < bandCenter - bandwidth) { // Too close to wall on left
 
-			WallFollowingLab.leftMotor.setSpeed(motorHigh);// Turn right
-			WallFollowingLab.rightMotor.setSpeed(0); // Stop motor for sharper turn
-			WallFollowingLab.leftMotor.forward();
-			WallFollowingLab.rightMotor.forward();
-
-		} else if (distance < minRangeDist) { // Robot too close to the wall on the left
-
-			WallFollowingLab.leftMotor.setSpeed(motorHigh);// Turn right
+			WallFollowingLab.leftMotor.setSpeed(motorHigh); // Turn Right
 			WallFollowingLab.rightMotor.setSpeed(motorLow);
 			WallFollowingLab.leftMotor.forward();
 			WallFollowingLab.rightMotor.forward();
-
-		} else if (maxRangeDist < distance) { // Robot too far from the wall on the left
-
-			WallFollowingLab.leftMotor.setSpeed(motorLow);// Turn left
-			WallFollowingLab.rightMotor.setSpeed(motorHigh);
+		}
+		// End EJ
+		/*else if( this.distance < 10) {
+			
+			WallFollowingLab.leftMotor.setSpeed(motorHigh); // Turn Right
+			WallFollowingLab.rightMotor.setSpeed(0); // Sharper turn
 			WallFollowingLab.leftMotor.forward();
 			WallFollowingLab.rightMotor.forward();
+			
+		}*/
 
-		}
+		/*
+		 * // TODO: there are some errors when it gets within the minimum range that the
+		 * // sensor can handle, the "dead width" where it will run into the wall if
+		 * (minRangeDist < distance && distance < maxRangeDist) { // Robot is in the
+		 * acceptable range
+		 * 
+		 * WallFollowingLab.leftMotor.setSpeed(motorHigh);
+		 * WallFollowingLab.rightMotor.setSpeed(motorHigh);
+		 * WallFollowingLab.leftMotor.forward(); WallFollowingLab.rightMotor.forward();
+		 * 
+		 * } else if (distance < tinyRangeDist) { // Robot way too close to the wall on
+		 * the left
+		 * 
+		 * WallFollowingLab.leftMotor.setSpeed(motorHigh);// Turn right
+		 * WallFollowingLab.rightMotor.setSpeed(0); // Stop motor for sharper turn
+		 * WallFollowingLab.leftMotor.forward(); WallFollowingLab.rightMotor.forward();
+		 * 
+		 * } else if (distance < minRangeDist) { // Robot too close to the wall on the
+		 * left
+		 * 
+		 * WallFollowingLab.leftMotor.setSpeed(motorHigh);// Turn right
+		 * WallFollowingLab.rightMotor.setSpeed(motorLow);
+		 * WallFollowingLab.leftMotor.forward(); WallFollowingLab.rightMotor.forward();
+		 * 
+		 * } else if (maxRangeDist < distance) { // Robot too far from the wall on the
+		 * left
+		 * 
+		 * WallFollowingLab.leftMotor.setSpeed(motorLow);// Turn left
+		 * WallFollowingLab.rightMotor.setSpeed(motorHigh);
+		 * WallFollowingLab.leftMotor.forward(); WallFollowingLab.rightMotor.forward();
+		 * 
+		 * }
+		 */
 	}
 
 	@Override
