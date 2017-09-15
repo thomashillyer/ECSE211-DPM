2d75321f926954970f332da34ac97e069fcff18c
Thomas Hillyer <thomashillyer@users.noreply.github.com>
2017-09-14 21:04:16 -0400

    PController Attempt 1

--- Lab1_Haykal_Hillyer/src/ca/mcgill/ecse211/wallfollowing/PController.java
+++ Lab1_Haykal_Hillyer/src/ca/mcgill/ecse211/wallfollowing/PController.java
@@ -4,56 +4,87 @@ import lejos.hardware.motor.EV3LargeRegulatedMotor;
 
 public class PController implements UltrasonicController {
 
-  /* Constants */
-  private static final int MOTOR_SPEED = 200;
-  private static final int FILTER_OUT = 20;
+	/* Constants */
+	private static final int MOTOR_SPEED = 200;
+	private static final int FILTER_OUT = 20;
 
-  private final int bandCenter;
-  private final int bandWidth;
-  private int distance;
-  private int filterControl;
+	private static final double PROPORTION_CONST = 1.4;
 
-  public PController(int bandCenter, int bandwidth) {
-    this.bandCenter = bandCenter;
-    this.bandWidth = bandwidth;
-    this.filterControl = 0;
+	private final int bandCenter;
+	private final int bandWidth;
+	private int distance;
+	private int filterControl;
 
-    WallFollowingLab.leftMotor.setSpeed(MOTOR_SPEED); // Initalize motor rolling forward
-    WallFollowingLab.rightMotor.setSpeed(MOTOR_SPEED);
-    WallFollowingLab.leftMotor.forward();
-    WallFollowingLab.rightMotor.forward();
-  }
+	public PController(int bandCenter, int bandwidth) {
+		this.bandCenter = bandCenter;
+		this.bandWidth = bandwidth;
+		this.filterControl = 0;
 
-  @Override
-  public void processUSData(int distance) {
+		WallFollowingLab.leftMotor.setSpeed(MOTOR_SPEED); // Initalize motor rolling forward
+		WallFollowingLab.rightMotor.setSpeed(MOTOR_SPEED);
+		WallFollowingLab.leftMotor.forward();
+		WallFollowingLab.rightMotor.forward();
+	}
 
-    // rudimentary filter - toss out invalid samples corresponding to null
-    // signal.
-    // (n.b. this was not included in the Bang-bang controller, but easily
-    // could have).
-    //
-    if (distance >= 255 && filterControl < FILTER_OUT) {
-      // bad value, do not set the distance var, however do increment the
-      // filter value
-      filterControl++;
-    } else if (distance >= 255) {
-      // We have repeated large values, so there must actually be nothing
-      // there: leave the distance alone
-      this.distance = distance;
-    } else {
-      // distance went below 255: reset filter and leave
-      // distance alone.
-      filterControl = 0;
-      this.distance = distance;
-    }
+	@Override
+	public void processUSData(int distance) {
 
-    // TODO: process a movement based on the us distance passed in (P style)
-  }
+		int diff, leftSpeed, rightSpeed;
+		
+		// rudimentary filter - toss out invalid samples corresponding to null
+		// signal.
+		// (n.b. this was not included in the Bang-bang controller, but easily
+		// could have).
+		//
+		if (distance >= 255 && filterControl < FILTER_OUT) {
+			// bad value, do not set the distance var, however do increment the
+			// filter value
+			filterControl++;
+		} else if (distance >= 255) {
+			// We have repeated large values, so there must actually be nothing
+			// there: leave the distance alone
+			this.distance = distance;
+		} else {
+			// distance went below 255: reset filter and leave
+			// distance alone.
+			filterControl = 0;
+			this.distance = distance;
+		}
 
+		// TODO: process a movement based on the us distance passed in (P style)
+		if (filterControl < 0) {
+			diff = calcCorrection(filterControl);
+			leftSpeed = MOTOR_SPEED + diff;
+			rightSpeed = MOTOR_SPEED - diff;
+			WallFollowingLab.leftMotor.setSpeed(leftSpeed); // Turn Right
+			WallFollowingLab.rightMotor.setSpeed(rightSpeed);
+			WallFollowingLab.leftMotor.forward();
+			WallFollowingLab.rightMotor.forward();
+		} else if (filterControl > 0) {
+			diff = calcCorrection(filterControl);
+			leftSpeed = MOTOR_SPEED - diff;
+			rightSpeed = MOTOR_SPEED + diff;
+			WallFollowingLab.leftMotor.setSpeed(leftSpeed); // Turn Right
+			WallFollowingLab.rightMotor.setSpeed(rightSpeed);
+			WallFollowingLab.leftMotor.forward();
+			WallFollowingLab.rightMotor.forward();
+		}
+	}
 
-  @Override
-  public int readUSDistance() {
-    return this.distance;
-  }
+	private int calcCorrection(int errorVal) {
+		int speedCorrection;
+		if (errorVal < 0)
+			errorVal = -errorVal;
+
+		speedCorrection = (int) (PROPORTION_CONST * errorVal);
+
+		return speedCorrection;
+
+	}
+
+	@Override
+	public int readUSDistance() {
+		return this.distance;
+	}
 
 }
