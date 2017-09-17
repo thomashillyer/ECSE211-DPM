f8dcc070a1f46c735ae5a7ff24bb54b2fdcfc61e
Thomas Hillyer <thomashillyer@users.noreply.github.com>
2017-09-17 17:43:15 -0400

    p controller

--- Lab1_Haykal_Hillyer/src/ca/mcgill/ecse211/wallfollowing/PController.java
+++ Lab1_Haykal_Hillyer/src/ca/mcgill/ecse211/wallfollowing/PController.java
@@ -8,7 +8,7 @@ public class PController implements UltrasonicController {
 	private static final int MOTOR_SPEED = 200;
 	private static final int FILTER_OUT = 20;
 
-	private static final double PROPORTION_CONST = 2;
+	private static final double PROPORTION_CONST = 5;
 
 	private final int bandCenter;
 	private final int bandWidth;
@@ -30,17 +30,17 @@ public class PController implements UltrasonicController {
 	public void processUSData(int distance) {
 
 		int diff, leftSpeed, rightSpeed;
-		
+
 		// rudimentary filter - toss out invalid samples corresponding to null
 		// signal.
 		// (n.b. this was not included in the Bang-bang controller, but easily
 		// could have).
 		//
-		if (distance >= 255 && filterControl < FILTER_OUT) {
+		if (distance >= 150 && filterControl < FILTER_OUT) {
 			// bad value, do not set the distance var, however do increment the
 			// filter value
 			filterControl++;
-		} else if (distance >= 255) {
+		} else if (distance >= 150) {
 			// We have repeated large values, so there must actually be nothing
 			// there: leave the distance alone
 			this.distance = distance;
@@ -50,21 +50,24 @@ public class PController implements UltrasonicController {
 			filterControl = 0;
 			this.distance = distance;
 		}
-		
+
+		if (this.distance <= 55) {
+			float floatDistance = this.distance / (float) 1.4;
+
+			this.distance = (int) floatDistance;
+		}
+
 		int distanceError = bandCenter - this.distance;
-		
-		
 
 		// TODO: process a movement based on the us distance passed in (P style)
-		if(Math.abs(distanceError) <= bandWidth) {
+		if (Math.abs(distanceError) <= bandWidth) {
 			leftSpeed = MOTOR_SPEED;
 			rightSpeed = MOTOR_SPEED;
 			WallFollowingLab.leftMotor.setSpeed(leftSpeed);
 			WallFollowingLab.rightMotor.setSpeed(rightSpeed);
 			WallFollowingLab.leftMotor.forward();
 			WallFollowingLab.rightMotor.forward();
-		}
-		else if (distanceError > 0) {
+		} else if (distanceError > 0) {
 			diff = calcCorrection(distanceError);
 			leftSpeed = MOTOR_SPEED + diff;
 			rightSpeed = MOTOR_SPEED - diff;
@@ -90,8 +93,9 @@ public class PController implements UltrasonicController {
 
 		speedCorrection = (int) (PROPORTION_CONST * errorVal);
 
-		if(speedCorrection >= MOTOR_SPEED) speedCorrection = 100;
-		
+		if (speedCorrection >= MOTOR_SPEED)
+			speedCorrection = 50;
+
 		return speedCorrection;
 
 	}
